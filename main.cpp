9a46dbe2ebe4025f63f89dad6b288463d204e735
EthanThomson-ltb <ltbluet@gmail.com>
2026-03-04 10:07:34 -0700

    Placeholder main script changed, added ability to insert a new user ID with insertNewID function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+void insertNewID(string IDarray[], int& sizeCount);
 
 void displayIDarray(string IDarray[], int sizeCount);
 
@@ -7,16 +10,28 @@ void displayIDarray(string IDarray[], int sizeCount);
 int main() {
 
     string IDarray[20];
-    int sizeCount = 0;
     for (int i = 0; i < 20; i++) {
         IDarray[i] = "EMPTY";
     }
-    cin >> sizeCount;
+    int sizeCount = 0;
+    insertNewID(IDarray, sizeCount);
+    insertNewID(IDarray, sizeCount);
+
     displayIDarray(IDarray, sizeCount);
 
-
-
     return 0;
+}
+
+void insertNewID(string IDarray[], int& sizeCount) {
+    if (sizeCount == 19) {
+        cout << "Maximum number of IDs reached" << endl << "Coming soon - Upgrade to maximum of 40 IDs for only $3.99/month" << endl;
+        return;
+    }
+    cout << "Enter new user ID in the ( UserID UserName ) format" << endl;
+    string inputID;
+    getline(cin, inputID);
+    IDarray[sizeCount] = inputID;
+    sizeCount++;
 }
 
 void displayIDarray(string IDarray[], int sizeCount) {
