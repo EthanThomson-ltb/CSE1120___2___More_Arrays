de6081dd145ada50b625d2295ceaca3c41f8f0c7
EthanThomson-ltb <ltbluet@gmail.com>
2026-03-04 09:29:01 -0700

    Placeholder main script added, ID display function in usable state.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void displayIDarray(string IDarray[], int sizeCount);
+
+
 int main() {
 
-    cout << "Hello, World!" << endl;
+    string IDarray[20];
+    int sizeCount = 0;
+    for (int i = 0; i < 20; i++) {
+        IDarray[i] = "EMPTY";
+    }
+    cin >> sizeCount;
+    displayIDarray(IDarray, sizeCount);
+
+
 
     return 0;
+}
+
+void displayIDarray(string IDarray[], int sizeCount) {
+    if (sizeCount == 0) {
+        cout << "No stored IDs found" << endl;
+        return;
+    }
+    for (int i = 0; i < sizeCount; i++) {
+        cout << "User ID Index[" << i << "] = " << IDarray[i] << endl;
+    }
 }
